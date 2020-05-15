4c2f496efcc741b40446f79d474ff179d067ae3b
BlackHeart <valepa007@gmail.com>
2020-05-15 19:55:47 +0300

    Add test login

--- src/components/Authorization/AuthorizationLogin.tsx
+++ src/components/Authorization/AuthorizationLogin.tsx
@@ -3,6 +3,7 @@ import {validateForm} from "../../components/validateForm/validateForm";
 // import {authorizationActionCreator, authorizationGoogleActionCreator} from "../../store/action/authorization";
 import {connect} from "react-redux";
 import {Form, Button, Container, Col, Row} from "react-bootstrap";
+import {TextField} from "@material-ui/core";
 
 export interface IState {
     email: {
@@ -37,17 +38,19 @@ export function AuthorizationLogin(props: IProps) {
         password: {
             status: false,
             message: '',
-            value: ''
+            value: '1'
         },
         shouldRemember: false
     }
 
     const [state, setState] = useState<IState>(initialState)
+    const [password, setPassword] = useState('');
     const [dialogOpened, setDialogOpened] = useState(false);
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
     const [errorForm, setError] = useState(initialState);
 
     const handleLogin = () => {
+        console.log(state.password.value)
         // props.action.authorization(state.email.value, state.password.value)
         handleClose()
     };
@@ -64,15 +67,13 @@ export function AuthorizationLogin(props: IProps) {
         setDialogOpened(false)
     };
 
-    // useEffect(() => {
-    //     if (!errorForm.email.status
-    //         && !errorForm.password.status
-    //         && email.trim() && password.trim()) {
-    //         setIsButtonDisabled(false);
-    //     } else {
-    //         setIsButtonDisabled(true);
-    //     }
-    // }, [email, password]);
+    useEffect(() => {
+        if (state.email.value.trim() && state.password.value.trim()) {
+            setIsButtonDisabled(false);
+        } else {
+            setIsButtonDisabled(true);
+        }
+    }, [state]);
 
     const handleKeyPress = (e: React.KeyboardEvent) => {
         if (e.keyCode === 13 || e.which === 13) {
@@ -81,33 +82,66 @@ export function AuthorizationLogin(props: IProps) {
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>, cb: any) => {
-        const {name, value} = e.currentTarget;
+        const {name, value} = e.target;
         const infoValid = validateForm(name, value)
         setError({...errorForm, [name]: {status: infoValid.error, message: infoValid.errorMessage}});
         return cb
     }
+
+    const handl = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    }
     return (
+
         <Form data-testid="login-form">
+            <div>{state.password.value}</div>
             <div style={{textAlign: "center"}}>
                 <strong>Login</strong>
             </div>
+            <TextField
+                error={errorForm.email.status}
+                helperText={errorForm.email.message}
+                variant="outlined"
+                fullWidth
+                id="email"
+                autoFocus
+                name="email"
+                type="email"
+                size={"small"}
+                label="Email"
+                placeholder="Email"
+                margin="normal"
+                value={state.email.value}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, setState({...state, email: {value: e.target.value, message: '', status: false}}))}
+                onKeyPress={(e)=>handleKeyPress(e)}
+            />
             <Form.Group controlId="formLogin">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    data-testid="email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, setState({...state, email: {value: e.target.value, message: '', status: false}}))}
-                    // onKeyPress={(e: any)=>handleKeyPress(e)}
-                />
+                {/*<Form.Control*/}
+                {/*    data-testid="email"*/}
+                {/*    name="email"*/}
+                {/*    type="email"*/}
+                {/*    placeholder="Email"*/}
+                {/*    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, setState({...state, email: {value: e.target.value, message: '', status: false}}))}*/}
+                {/*    // onKeyPress={(e: any)=>handleKeyPress(e)}*/}
+                {/*/>*/}
                 <br />
                 <Form.Label>Password</Form.Label>
+                {/*<input*/}
+                {/*    id={'password'} // изменили на id*/}
+                {/*    type={'password'}*/}
+                {/*    value*/}
+                {/*    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, setState({...state, password: {value: e.currentTarget.value, message: '', status: false}}))}*/}
+
+                {/*    placeholder={'Пароль'}*/}
+                {/*    value={state.password.value}*/}
+                {/*/>*/}
                 <Form.Control
                     data-testid="password"
                     name="password"
                     type="password"
+                    value={state.password.value}
                     placeholder="Password"
+                    // onChange={handl}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, setState({...state, password: {value: e.target.value, message: '', status: false}}))}
                     // onKeyPress={(e)=>handleKeyPress(e)}
                 />
@@ -127,7 +161,8 @@ export function AuthorizationLogin(props: IProps) {
                 block
                 variant={"primary"}
                 onClick={handleLogin}
-                disabled={isButtonDisabled}>
+                disabled={isButtonDisabled}
+            >
                 LOGIN
             </Button>
             <Container>
---
+++ src/components/Authorization/__test__/SignUp.unit.test.tsx
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+import {AuthorizationLogin, IProps} from "../AuthorizationLogin"
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import {shallow} from "enzyme";
+
+
+describe('<AuthorizationLogin/>',  () => {
+    const mockLogin = jest.fn()
+    const password = '4TYE46E8RT!@qw'
+    const name = 'login'
+    const wrongPassword = '11'
+    const email = 'vas@gmail.com'
+    const wrongEmail = 'vascom'
+
+    const login = shallow<Component>(<AuthorizationLogin shouldRemember={false} onChangeForm={(name: string) => mockLogin(name)} onHideModal={mockLogin}/>)
+
+    it('renders properly', () => {
+        expect(login).toMatchSnapshot()
+    })
+
+    it('render firm with disabled button at initial render', ()=>{
+        expect(login.find('Button').prop('disabled')).toBeTruthy()
+    })
+
+    // it('render firm with disabled button at initial render', ()=>{
+    //
+    //     login.find('[type="password"]').simulate('change', {
+    //         target: {
+    //             name: 'password',
+    //             value: password
+    //         }
+    //     })
+    //     login.find('[type="email"]').simulate('change', {
+    //         target: {
+    //             name: 'email',
+    //             value: email
+    //         }
+    //     })
+    //     console.log(login.find('[type="email"]').prop('value'))
+    //     console.log(login.find('[type="password"]').prop('value'))
+    //     expect(login.find('Button').prop('disabled')).toBeFalsy()
+    // })
+
+    it('should set the password value on change event with trim', () => {
+        login.find('[type="password"]').simulate('change', {
+            target: {
+                name: 'password',
+                value: password,
+            },
+        });
+        expect(login.find('[type="password"]').prop('value')).toEqual(
+            password,
+        );
+    });
+
+    it('should set the email value on change event with trim', () => {
+        login.find('[type="email"]').simulate('change', {
+            target: {
+                name: 'email',
+                value: email,
+            },
+        });
+        expect(login.find('[type="email"]').prop('value')).toEqual(
+            email,
+        );
+    });
+    //
+    // it('should set wrong email value', () => {
+    //     login.find('[type="email"]').simulate('change', {
+    //         target: {
+    //             name: 'email',
+    //             value: wrongEmail,
+    //         },
+    //     });
+    //     expect(login.find('[type="email"]').prop('value')).toEqual(
+    //         email,
+    //     );
+    // });
+    // it('should set wrong password value', () => {
+    //     login.find('[type="password"]').simulate('change', {
+    //         target: {
+    //             name: 'password',
+    //             value: wrongPassword,
+    //         },
+    //     });
+    //     expect(login.find('[type="password"]').prop('value')).toEqual(
+    //         password,
+    //     );
+    // });
+
+})
